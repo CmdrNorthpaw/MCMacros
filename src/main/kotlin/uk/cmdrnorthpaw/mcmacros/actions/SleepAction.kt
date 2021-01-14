41ea22939dbd2b00480dce9709dc21f8a8c3d9ec
CmdrNorthpaw <thetuxkid@gmail.com>
2021-01-14 12:28:00 +0000

    Make Actions return ActionResults

--- src/main/kotlin/uk/cmdrnorthpaw/mcmacros/actions/SleepAction.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/mcmacros/actions/SleepAction.kt
@@ -4,10 +4,11 @@ import kotlinx.coroutines.delay
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.util.ActionResult
 import uk.cmdrnorthpaw.mcmacros.model.Action
 
 @Serializable
 @SerialName("sleep")
 class SleepAction(val time: Int) : Action() {
-    override suspend fun act(player: ClientPlayerEntity) = delay(time * 50L)
+    override suspend fun act(player: ClientPlayerEntity): ActionResult { delay(time * 50L); return ActionResult.PASS }
 }
--- src/main/kotlin/uk/cmdrnorthpaw/mcmacros/model/Action.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/mcmacros/model/Action.kt
@@ -2,8 +2,9 @@ package uk.cmdrnorthpaw.mcmacros.model
 
 import kotlinx.serialization.Serializable
 import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.util.ActionResult
 
 @Serializable
 abstract class Action {
-    abstract suspend fun act(player: ClientPlayerEntity)
+    abstract suspend fun act(player: ClientPlayerEntity): ActionResult
 }
