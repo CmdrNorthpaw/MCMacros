959de18f87923e0a7392b2db9ded3a5d172a1250
CmdrNorthpaw <thetuxkid@gmail.com>
2021-01-14 12:19:17 +0000

    I hope Action is done now

--- src/main/kotlin/uk/cmdrnorthpaw/mcmacros/model/Action.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/mcmacros/model/Action.kt
@@ -1,6 +1,9 @@
 package uk.cmdrnorthpaw.mcmacros.model
 
 import kotlinx.serialization.Serializable
+import net.minecraft.client.network.ClientPlayerEntity
 
 @Serializable
-abstract class Action()
+abstract class Action {
+    abstract suspend fun act(player: ClientPlayerEntity)
+}
