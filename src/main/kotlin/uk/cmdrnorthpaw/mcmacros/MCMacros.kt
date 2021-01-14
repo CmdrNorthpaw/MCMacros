81b8e27ed26230fba05a0e37bdfe70ac19e498f5
CmdrNorthpaw <thetuxkid@gmail.com>
2021-01-14 15:15:07 +0000

    Add list of macros to be parsed from the filesystem

--- src/main/kotlin/uk/cmdrnorthpaw/mcmacros/MCMacros.kt
+++ src/main/kotlin/uk/cmdrnorthpaw/mcmacros/MCMacros.kt
@@ -1,23 +1,36 @@
 package uk.cmdrnorthpaw.mcmacros
 
+import com.charleskorn.kaml.Yaml
+import kotlinx.serialization.decodeFromString
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.loader.api.FabricLoader
+import uk.cmdrnorthpaw.mcmacros.model.Macro
 import java.io.File
 
 object MCMacros : ModInitializer {
     override fun onInitialize() {
         TODO("Not yet implemented")
     }
-
-    val macrosDir = File(FabricLoader.getInstance().gameDir.toFile(), "macros").mkdir()
 }
 
 @Environment(EnvType.CLIENT)
 object MCMacrosClient : ClientModInitializer {
     override fun onInitializeClient() {
-        TODO("Not yet implemented")
+        Macros = getMacroList()
     }
+
+    private fun getMacroList(): Array<Macro> {
+        val macroList = mutableListOf<Macro>()
+        macrosDir.list()?.forEach {
+            val file = File(macrosDir, it)
+            if (file.name.endsWith(".yml")) macroList.add(Yaml.default.decodeFromString(Macro.serializer(), file.readText()))
+        }
+        return macroList.toTypedArray()
+    }
+    val macrosDir = File(FabricLoader.getInstance().gameDir.toFile(), "macros")
 }
+
+lateinit var Macros: Array<Macro>
